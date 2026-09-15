9a50f6fcf8eddd7f0b667d729cca4096d3d0d29c
beniamino green <beniamino.green@tuta.com>
2026-09-15 12:00:51 -0400

    added interpretation helper function

--- R/rv.R
+++ R/rv.R
@@ -53,3 +53,63 @@ robustness_values <- function(w, z, y) {
 
   return(bind_rows(soo_df, iv_df))
 }
+
+
+#' Interpret a Table Of Robustness Values
+#' 
+#'
+#' Struggling to interpret the results of `robustness_values`? Look no further! 
+#' `interpret_table` helps by providing a plain-text interpretation of the robustness 
+#' values of a design. It interprets each of the four robustness values for you, 
+#' and flags low robustness values. 
+#'
+#' @param df a datasdrame, as returned by `robustness_values`
+#'
+#' @return A string 
+interpret_table <- function(df) {
+  paragraphs <- lapply(seq_len(nrow(df)), function(i) {
+    rv <- signif(df$rv[i], 3)
+    bias <- signif(df$bias[i], 3)
+    pct <- signif(100 * df$rv[i], 3)
+
+    warn <- (df$rv[i] < .05)
+
+    out <- switch(tolower(as.character(df$method[i])),
+      soo = stringr::str_glue(
+        "The robustness value for selection on observables is {rv}. ",
+        "A bias of {bias} requires an unobserved confounder to explain ",
+        "at least {pct}% of residual variation in at least one of ",
+        "the treatment or the outcome."
+      ),
+      iv = stringr::str_glue(
+        "The robustness value for instrumental variables is {rv}. ",
+        "A bias of {bias} requires either the confounder to explain ",
+        "at least {pct}% of residual instrument variation, or the ",
+        "instrument to explain at least {pct}% of outcome variation ",
+        "remaining after adjusting for the treatment and confounder."
+      ),
+      exogenousiv = stringr::str_glue(
+        "With instrument exogeneity imposed, the robustness value is {rv}. ",
+        "A bias of {bias} requires the instrument to explain at least ",
+        "{pct}% of outcome variation remaining after adjusting for ",
+        "the treatment and confounder."
+      ),
+      excludableiv = stringr::str_glue(
+        "With exclusion imposed, the robustness value is {rv}. ",
+        "A bias of {bias} requires the confounder to explain at least ",
+        "{pct}% of residual instrument variation."
+      ),
+      NULL
+    )
+
+    if (warn) {
+      return(paste(out, 
+        "This robustness value is below 5%. While there is no 'hard limit' on these values, a value this small means that relatively small violations of the corresponding identifying assumptions could drive the point estimate to zero."))
+    } else {
+      return(out)
+    }
+  })
+
+  paste(c(unlist(paragraphs, use.names = FALSE), context), collapse = "\n\n")
+}
+
